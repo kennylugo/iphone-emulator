11a42f180e4287f1df2039c7be6f660982e8ec79
kennybatista <kennybatista7@gmail.com>
2015-09-07 08:52:55 -0400

    organizing

---
+++ main.js
@@ -0,0 +1,445 @@
+
+    var mySwiper = new Swiper ('.swiper-container', {
+                // Optional parameters
+                direction: 'horizontal',
+                loop: false,
+                
+                // If we need pagination
+                pagination: '.swiper-pagination',
+                
+                // // Navigation arrows
+                // nextButton: '.swiper-button-next',
+                // prevButton: '.swiper-button-prev',
+                
+                // And if we need scrollbar
+                // scrollbar: '.swiper-scrollbar',
+              })   
+
+
+
+
+
+
+              $(document).ready(function(e){
+
+
+
+
+
+
+              
+
+
+
+                    //show modal function
+                function showModal1(){
+                    $(".modal-wrapper-1").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal1() {
+                    $(".modal-wrapper-1").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-1").click(function(e){
+                    showModal1(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal1(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+                    //show modal function
+                function showModal2(){
+                    $(".modal-wrapper-2").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal2() {
+                    $(".modal-wrapper-2").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-2").click(function(e){
+                    showModal2(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal2(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+                    //show modal function
+                function showModal3(){
+                    $(".modal-wrapper-3").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal3() {
+                    $(".modal-wrapper-3").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-3").click(function(e){
+                    showModal3(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal3(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+                    //show modal function
+                function showModal4(){
+                    $(".modal-wrapper-4").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal4() {
+                    $(".modal-wrapper-4").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-4").click(function(e){
+                    showModal4(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal4(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+                    //show modal function
+                function showModal5(){
+                    $(".modal-wrapper-5").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal5() {
+                    $(".modal-wrapper-5").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-5").click(function(e){
+                    showModal5(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal5(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //show modal function
+                function showModal6(){
+                    $(".modal-wrapper-6").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal6() {
+                    $(".modal-wrapper-6").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-6").click(function(e){
+                    showModal6(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal6(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //show modal function
+                function showModal7(){
+                    $(".modal-wrapper-7").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal7() {
+                    $(".modal-wrapper-7").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-7").click(function(e){
+                    showModal7(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal7(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+
+
+
+                    //show modal function
+                function showModal8(){
+                    $(".modal-wrapper-8").show();
+                    
+                }
+
+
+
+                //hide the modal function
+                function hideModal8() {
+                    $(".modal-wrapper-8").hide();
+
+                    
+                }
+
+
+
+
+                $("#app-8").click(function(e){
+                    showModal8(); //show the modal method.
+                    e.preventDefault();
+                });
+
+
+                //when button exit and no are clicked it will close the modal
+                $(".close-modal").click(function(e){
+                    hideModal8(); //hide the modal method.
+                    e.preventDefault();
+                });
+
+                
+
+
+
+
+            });
